a83c3ae102cae2cd3c6ead57180cdaaa78519cae
GabFur <caminhaocagao@gmail.com>
2026-06-06 13:19:13 -0300

    Alteração completa no portfólio
    Mudança do layout do html e de seu conteúdo (index.html)
    Mudança no estilo (style.css)
    Mudança no script (script.js)
    Alteração no caminho de pastas

--- arquivos-script/script.js
+++ arquivos-script/script.js
@@ -1,23 +1,9 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
+const projeto = document.querySelector(".projeto");
 
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        if (targetElement) {
-            window.scrollTo({
-                top: targetElement.offsetTop - 70, behavior: 'smooth'
-            });
-        }
-    });
+projeto.addEventListener("mouseenter", () => {
+    projeto.style.transform = "scale(1.03)";
 });
 
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) {
-        header.style.padding = '0.5rem 0';
-    } else {
-        header.style.padding = '1rem 0';
-    }
+projeto.addEventListener("mouseleave", () => {
+    projeto.style.transform = "scale(1)";
 });
